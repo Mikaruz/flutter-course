97362e99ab972acd319772eb772066c3e669a78c
Mikaruz <gersonsebas2019@gmail.com>
2023-08-02 12:26:56 -0500

    Botones Completados

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -62,8 +62,34 @@ class _ButtonsView extends StatelessWidget {
                 icon: const Icon(Icons.app_registration_rounded),
                 style: ButtonStyle(
                     backgroundColor: MaterialStatePropertyAll(colors.primary),
-                    iconColor: const MaterialStatePropertyAll(Colors.white)))
+                    iconColor: const MaterialStatePropertyAll(Colors.white))),
+            const CustomButton()
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text(
+                'Custom',
+                style: TextStyle(color: Colors.white),
+              )),
         ),
       ),
     );
