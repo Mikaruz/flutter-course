4139634cbc601e1d6d4d7e246a14ba614d03fc79
Mikaruz <gersonsebas2019@gmail.com>
2023-09-02 16:25:47 -0500

    Formulario creado

---
+++ lib/infrastructure/inputs/inputs.dart
@@ -0,0 +1,3 @@
+export 'package:widgets_app/infrastructure/inputs/username.dart';
+export 'package:widgets_app/infrastructure/inputs/password.dart';
+export 'package:widgets_app/infrastructure/inputs/email.dart';
--- lib/presentation/screens/login/login_screen.dart
+++ lib/presentation/screens/login/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:widgets_app/presentation/blocs/register/register_cubit.dart';
 import 'package:widgets_app/presentation/screens/login/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -10,7 +12,8 @@ class LoginScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Login Screen'),
       ),
-      body: const _RegisterView(),
+      body: BlocProvider(
+          create: (context) => RegisterCubit(), child: const _RegisterView()),
     );
   }
 }
@@ -38,68 +41,41 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatefulWidget {
+class _RegisterForm extends StatelessWidget {
   const _RegisterForm();
 
   @override
-  State<_RegisterForm> createState() => _RegisterFormState();
-}
-
-class _RegisterFormState extends State<_RegisterForm> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  String username = '';
-  String email = '';
-  String password = '';
-
-  @override
   Widget build(BuildContext context) {
+    final registerCubit = context.watch<RegisterCubit>();
+    final username = registerCubit.state.username;
+    final password = registerCubit.state.password;
+    final email = registerCubit.state.email;
+
     return Form(
-      key: _formKey,
       child: Column(children: [
-        SizedBox(height: 25),
+        const SizedBox(height: 25),
         CustomTextFormField(
           label: 'Nombre de usuario',
-          onChanged: (value) => username = value,
-          validator: (value) {
-            if (value == null || value.isEmpty) return 'Campo requerido';
-            if (value.trim().isEmpty) return 'Campo requerido';
-            if (value.length < 6) return 'Más de 6 letras';
-            return null;
-          },
+          onChanged: registerCubit.usernameChanged,
+          errorMessage: username.errorMessage,
         ),
-        SizedBox(height: 15),
+        const SizedBox(height: 15),
         CustomTextFormField(
           label: 'Correo electrónico',
-          onChanged: (value) => email = value,
-          validator: (value) {
-            if (value == null || value.isEmpty) return 'Campo requerido';
-            if (value.trim().isEmpty) return 'Campo requerido';
-            final emailRegExp = RegExp(
-              r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
-            );
-            if (!emailRegExp.hasMatch(value)) return 'Ingrese un correo válido';
-
-            return null;
-          },
+          onChanged: registerCubit.emailChanged,
+          errorMessage: email.errorMessage,
         ),
-        SizedBox(height: 15),
+        const SizedBox(height: 15),
         CustomTextFormField(
           label: 'Contraseña',
           obscureText: true,
-          onChanged: (value) => password = value,
-          validator: (value) {
-            if (value == null || value.isEmpty) return 'Campo requerido';
-            if (value.trim().isEmpty) return 'Campo requerido';
-            if (value.length < 6) return 'Más de 6 letras';
-            return null;
-          },
+          onChanged: registerCubit.passwordChanged,
+          errorMessage: password.errorMessage,
         ),
         const SizedBox(height: 20),
         FilledButton.tonalIcon(
             onPressed: () {
-              final isValid = _formKey.currentState!.validate();
-              if (!isValid) return;
-              print('$username, $email, $password');
+              registerCubit.onSubmit();
             },
             icon: const Icon(Icons.save),
             label: const Text('Crear usuario'))
